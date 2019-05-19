b17f0b1128a83bc508e4993984f57fe4417522ec
yalh76 <yalh@yahoo.com>
2019-05-19 18:19:17 +0200

    Apply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="postgresql libstdc++6 redis-server rabbitmq-server"
+
+#=================================================
 # PERSONAL HELPERS
 #=================================================
 source="https://github.com/ONLYOFFICE/DocumentServer/releases/download/ONLYOFFICE-DocumentServer-5.2.8/onlyoffice-documentserver_amd64.deb"
