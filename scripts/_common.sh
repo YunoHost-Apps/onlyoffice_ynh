ad4f3a2503b342c5fab45743f500a28d0ab3eaf1
yalh76 <yalh@yahoo.com>
2019-05-22 02:43:24 +0200

    Reapply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,13 +5,16 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql libstdc++6 redis-server rabbitmq-server"
+pkg_dependencies="postgresql libstdc++6 redis-server rabbitmq-server libcurl4-nss-dev"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-source="https://github.com/ONLYOFFICE/DocumentServer/releases/download/ONLYOFFICE-DocumentServer-5.2.8/onlyoffice-documentserver_amd64.deb"
-version="5.2.8"
+
+onlyoffice_version="5.2.8"
+
+onlyoffice_source="https://github.com/ONLYOFFICE/DocumentServer/releases/download/ONLYOFFICE-DocumentServer-$onlyoffice_version/onlyoffice-documentserver_amd64.deb"
+
 
 #=================================================
 # EXPERIMENTAL HELPERS
