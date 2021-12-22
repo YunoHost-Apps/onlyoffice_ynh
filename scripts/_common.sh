d16459d86cca628dd5ab23a0c51cb723348881c6
Gredin67 <gredin67@sans-nuage.fr>
2021-12-22 21:58:07 +0100

    =6.4.2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="postgresql postgresql-contrib libstdc++6 rabbitmq-server libcurl4-dev"
-extra_dependencies="onlyoffice-documentserver>=6.4.2"
+extra_dependencies="onlyoffice-documentserver=6.4.2"
 
 #=================================================
 # PERSONAL HELPERS
