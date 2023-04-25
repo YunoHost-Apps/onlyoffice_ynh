f3f095544a43218b6c869b17935757477bfd6454
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-25 09:06:50 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,9 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-contrib libstdc++6 rabbitmq-server libcurl4-dev"
-contrib_dependencies="ttf-mscorefonts-installer"
-extra_dependencies="onlyoffice-documentserver"
+#REMOVEME? pkg_dependencies="postgresql postgresql-contrib libstdc++6 rabbitmq-server libcurl4-dev"
+#REMOVEME? contrib_dependencies="ttf-mscorefonts-installer"
+#REMOVEME? extra_dependencies="onlyoffice-documentserver"
 
 #=================================================
 # PERSONAL HELPERS
