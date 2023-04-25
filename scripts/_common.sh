8b2969f53dcebf36211a7037726e623cdc943b73
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-25 09:22:49 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql postgresql-contrib libstdc++6 rabbitmq-server libcurl4-dev"
-#REMOVEME? contrib_dependencies="ttf-mscorefonts-installer"
-#REMOVEME? extra_dependencies="onlyoffice-documentserver"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
