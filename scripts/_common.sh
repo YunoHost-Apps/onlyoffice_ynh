6e989dcd6f386ceb459206bfa46a241ce49bd90b
otm33 <b.arrierg@orange.fr>
2025-10-24 12:41:23 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,7 +16,7 @@ init_settings() {
     ynh_app_setting_set_default --key=autoassembly_enable --value="true"
     ynh_app_setting_set_default --key=autoassembly_step --value="1m"
     ynh_app_setting_set_default --key=autoassembly_interval --value="3m"
-    autoassembly_enable=true
+
     # ynh_add_config doesn't transform 0 into false and 1 into true...
     ynh_app_setting_set_default --key=reject_unauthorized --value="true"
     if [[ "$reject_unauthorized" == "0" ]]; then
